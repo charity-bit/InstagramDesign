c3ca4cf3f1e8bd6871e3d8bd1b1275763befd798
charity-bit <charitynyanchera@gmail.com>
2020-09-01 11:54:21 +0300

    On branch master

--- InstagramDesign/InstagramDesign/AssemblyInfo.cs
+++ InstagramDesign/InstagramDesign/AssemblyInfo.cs
@@ -1,3 +1,8 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("BillaBong.ttf",Alias ="billa")]
+[assembly: ExportFont("SF-Pro-Display-Medium.ttf", Alias = "display")]
+[assembly: ExportFont("SF-Pro-Text.ttf", Alias = "text")]
+[assembly: ExportFont("Nexa.ttf", Alias = "nexa")]
